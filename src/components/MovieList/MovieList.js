ec19b9aa8e84b3b93c45de6a432b8df041eb1745
Dvtbyhn <beyhandavut@gmail.com>
2022-10-25 18:32:30 +0300

    edit

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -7,8 +7,6 @@ import "./style.css"
 
 export default function MovieList(props) {
 
- 
-
   const truncateOverview = (string, maxLength) => {
     if (!string) return null;
     if (string.length <= maxLength) return string;
@@ -16,55 +14,37 @@ export default function MovieList(props) {
   }
 
 
-
-
-
-
   return (
 
     <>
-    
-      <div className='container mt-4'>
-        <div className='row'>
+ <div className='container mt-4'>
+   <div className='row'>
 
-          {
-            props.filteredMovies.map((movie, i) => {
-              return (
-                <div key={i} className='col-xs-12 col-sm-12 col-md-6 col-lg-4 g-5'>
-                  <div className="card" style={{
-                    
-                    boxShadow: "5px 4px 8px gray,  5px 5px 18px black ,2px 3px 4px 4px green"
-                  }}>
-                    <img src={movie.imageURL} className="card-img-top" alt={movie.name} />
-                    <div className="card-body">
-                      <h5 className="card-title"> {movie.name} </h5>
-                      <p className="card-text"> {truncateOverview(movie.overview, 100)} </p>
-                      <div className='d-flex justify-content-between'>
-                        <button className="btn btn-light" disabled> {movie.rating} </button>
-                        <button className='btn btn-light' disabled>{movie.kind} </button>
-                      
-                      </div>
-                    </div>
+{
+  props.filteredMovies.map((movie, i) => {
+    return (
+ <div key={i} className='col-xs-12 col-sm-12 col-md-6 col-lg-4 g-5'>
+   <div className="card" style={{
 
-                  </div>
-                </div>
+boxShadow: "5px 4px 8px gray,  5px 5px 18px black ,2px 3px 4px 4px green"
+   }}>
+<img src={movie.imageURL} className="card-img-top" alt={movie.name} />
+<div className="card-body">
+  <h5 className="card-title"> {movie.name} </h5>
+  <p className="card-text"> {truncateOverview(movie.overview, 100)} </p>
+  <div className='d-flex justify-content-between'>
+    <button className="btn btn-light" disabled> {movie.rating} </button>
+    <button className='btn btn-light' disabled>{movie.kind} </button>
+  
+  </div>
+  </div>
+ </div>
+ </div>
+    )
+  })}
 
-              )
-            })}
-
-
-
-        </div>
-      </div>
-
-
-
-
-
-
-
-
-
+  </div>
+  </div>
     </>
   )
 }
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -16,155 +16,136 @@ export default function SignUp() {
     return (
         <>
 
-            <div  >
+  <div>
+  </div>
 
-            </div>
-
-            <div className='container-sign'>
-                <div className="card-sign mt-1 ">
-                    <div className="body-sign">
-                        <NavLink className="navLink" to={"/"}> Giriş Sayfasına Dön </NavLink>
-
-                        <h3 >Yeni Kayıt Formu</h3>
-                        <Formik
-                            initialValues={{
-                                fullName: "",
-                                password: "",
-                                confirmPassword: "",
-                                userName: "",
-                                email: ""
-                            }}
-                            validationSchema={Yup.object({
-                                fullName: Yup.string().required("İsim boş bırakılamaz"),
-                                password: Yup.string().required("Lütfen şifre oluşturun").min(5, "Şifreniz çok kısa"),
-                                confirmPassword: Yup.string().oneOf([Yup.ref("password"), null], "Şifreler aynı olmalı"),
-                                userName: Yup.string().required("Kullanıcı adı oluşturun").min(7, "Kullanıcı adı çok kısa"),
-                                email: Yup.string().required("")
+  <div className='container-sign'>
+      <div className="card-sign mt-1 ">
+<div className="body-sign">
+    <NavLink className="navLink" to={"/"}> Giriş Sayfasına Dön </NavLink>
+    <h3 >Yeni Kayıt Formu</h3>
+              <Formik
+initialValues={{
+    fullName: "",
+    password: "",
+    confirmPassword: "",
+    userName: "",
+    email: ""
+}}
+validationSchema={Yup.object({
+    fullName: Yup.string().required("İsim boş bırakılamaz"),
+    password: Yup.string().required("Lütfen şifre oluşturun").min(5, "Şifreniz çok kısa"),
+    confirmPassword: Yup.string().oneOf([Yup.ref("password"), null], "Şifreler aynı olmalı"),
+    userName: Yup.string().required("Kullanıcı adı oluşturun").min(7, "Kullanıcı adı çok kısa"),
+    email: Yup.string().required("")
 
 
-                            })}
+})}
 
-                            onSubmit={(values, { resetForm, setSubmitting }) => {
-                                setSuccessAlert(<div className="alert  mt-2" role="alert">
-                                    Kaydınız oluşmuştur. İyi günler
-                                </div>)
-                                setTimeout(() => {
-                                    resetForm();
-                                    setSubmitting(false)
-                                    setSuccessAlert(false)
-                                }, 3000)
-                            }}
+onSubmit={(values, { resetForm, setSubmitting }) => {
+    setSuccessAlert(<div className="alert  mt-2" role="alert">
+        Kaydınız oluşmuştur. İyi günler
+    </div>)
+    setTimeout(() => {
+        resetForm();
+        setSubmitting(false)
+        setSuccessAlert(false)
+    }, 3000)
+}} >
+{({ values, errors, handleChange, touched, handleSubmit, handleReset, isSubmitting, dirty }) => (
 
-                        >
-                            {({ values, errors, handleChange, touched, handleSubmit, handleReset, isSubmitting, dirty }) => (
-
-                                <form onSubmit={handleSubmit}>
-                                    <div className="mb-3">
-                                        <label htmlFor="exampleInput1"
-                                            className="form-label">Ad Soyad</label>
-                                        <input
-                                            name='fullName'
-                                            type="text"
-                                            className="form-control"
-                                            id="fullName"
-                                            value={values.fullName}
-                                            onChange={handleChange}
-                                        />
-                                        {errors.fullName && touched.fullName && (
-                                            <div className='error-input' style={{ color: "red" }}>{errors.fullName}   </div>
-                                        )}
+    <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+   <label htmlFor="exampleInput1"
+       className="form-label">Ad Soyad</label>
+   <input
+       name='fullName'
+       type="text"
+       className="form-control"
+       id="fullName"
+       value={values.fullName}
+       onChange={handleChange}
+   />
+   {errors.fullName && touched.fullName && (
+       <div className='error-input' style={{ color: "red" }}>{errors.fullName}   </div>
+   )}
 
 
-                                    </div>
+        </div>
 
-                                    <div className="mb-3">
+        <div className="mb-3">
 
-                                        <label htmlFor="exampleInputPassword1"
-                                            className="form-label">Şifre Oluştur</label>
+   <label htmlFor="exampleInputPassword1"
+       className="form-label">Şifre Oluştur</label>
 
-                                        <input
-                                            name='password'
-                                            type="password"
-                                            className="form-control"
-                                            id="password"
-                                            value={values.password}
-                                            onChange={handleChange}
-                                            required />
-                                        {errors.password && touched.password && (
-                                            <div className='error-input' style={{ color: "red" }}>{errors.password}   </div>
-                                        )}
-                                    </div>
+   <input
+       name='password'
+       type="password"
+       className="form-control"
+       id="password"
+       value={values.password}
+       onChange={handleChange}
+       required />
+   {errors.password && touched.password && (
+       <div className='error-input' style={{ color: "red" }}>{errors.password}   </div>
+   )}
+        </div>
 
-                                    <div className="mb-3">
-                                        <label htmlFor="exampleInputPassword2"
-                                            className="form-label">Tekrar Şifre</label>
+        <div className="mb-3">
+   <label htmlFor="exampleInputPassword2"
+       className="form-label">Tekrar Şifre</label>
 
-                                        <input
-                                            name='confirmPassword'
-                                            type="password"
-                                            className="form-control"
-                                            id="confirmPassword"
-                                            value={values.confirmPassword}
-                                            onChange={handleChange}
-                                            required />
-                                        {errors.confirmPassword && touched.confirmPassword && (
-                                            <div className='error-input' style={{ color: "red" }}>{errors.confirmPassword}   </div>
-                                        )}
-                                    </div>
+   <input
+       name='confirmPassword'
+       type="password"
+       className="form-control"
+       id="confirmPassword"
+       value={values.confirmPassword}
+       onChange={handleChange}
+       required />
+   {errors.confirmPassword && touched.confirmPassword && (
+       <div className='error-input' style={{ color: "red" }}>{errors.confirmPassword}   </div>
+   )}
+        </div>
 
-                                    <div className="mb-3">
-                                        <label htmlFor="exampleInput2"
-                                            className="form-label">Kullanıcı Adı </label>
+        <div className="mb-3">
+   <label htmlFor="exampleInput2"
+       className="form-label">Kullanıcı Adı </label>
 
-                                        <input
-                                            name='userName'
-                                            type="text"
-                                            className="form-control"
-                                            id="userName"
-                                            value={values.userName}
-                                            onChange={handleChange}
-                                            required />
-                                        {errors.userName && touched.userName && (
-                                            <div className='error-input' style={{ color: "red" }}>{errors.userName}   </div>
-                                        )}
-                                    </div>
+   <input
+       name='userName'
+       type="text"
+       className="form-control"
+       id="userName"
+       value={values.userName}
+       onChange={handleChange}
+       required />
+   {errors.userName && touched.userName && (
+       <div className='error-input' style={{ color: "red" }}>{errors.userName}   </div>
+   )}
+        </div>
 
-                                    <div className="mb-3">
-                                        <label htmlFor="exampleInput2"
-                                            className="form-label">Email</label>
+        <div className="mb-3">
+   <label htmlFor="exampleInput2"
+       className="form-label">Email</label>
 
-                                        <input
-                                            name='email'
-                                            type="email"
-                                            className="form-control"
-                                            id="email"
-                                            value={values.email}
-                                            onChange={handleChange}
-                                            required />
-
-
-                                    </div>
-
-                                    <button type='submit' className='btn btn-success' disabled={!dirty || isSubmitting} >Kayıt Ol</button>
-
-                                </form>
-
-
-                            )}
-
-                        </Formik>
-                        <div> {successAlert} </div>
-
-
-
-
-
-
-
-
-                    </div>
-                </div>
-            </div>
+   <input
+       name='email'
+       type="email"
+       className="form-control"
+       id="email"
+       value={values.email}
+       onChange={handleChange}
+       required />
+        </div>
+        <button type='submit' className='btn btn-success' disabled={!dirty || isSubmitting} >Kayıt Ol</button>
+    </form>
+)}
+    </Formik>
+    <div> {successAlert} </div>
+</div>
+     </div>
+        </div>
         </>
     )
 }
