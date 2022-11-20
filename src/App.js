915bc47788396af4f436d4f6fa87832a2d76a9d6
Dvtbyhn <beyhandavut@gmail.com>
2022-11-20 13:15:58 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -14,53 +14,52 @@ import Loading from './components/Loading';
 
 export default function App() {
 
-
   const [film, setFilm] = useState([])
   const [search, setSearch] = useState("")
   const [loading, setLoading] = useState(false)
   const [favorite, setFavorite] = useState([])
   const [selectedCategory, setSelectedCategory] = useState();
-  const [comments,setComments] = useState("")
-  const [movieComment,setMovieComment] = useState([])
-
+  const [comments, setComments] = useState("")
+  const [movieComment, setMovieComment] = useState([])
 
   useEffect(() => {
+
     const get = JSON.parse(localStorage.getItem("favorites"))
     setFavorite(get)
-  }, [])
-
-  useEffect(() => {
     getMovies()
+
   }, [])
 
+
   useEffect(() => {
-     localStorage.setItem("favorites", JSON.stringify(favorite))
-  })
+    localStorage.setItem("favorites", JSON.stringify(favorite))
+  }, [favorite])
 
   useEffect(() => {
     const comment = JSON.parse(localStorage.getItem("movComment"))
-    setMovieComment (comment );
-  },[])
+    setMovieComment(comment);
+  }, [])
 
 
   useEffect(() => {
-    localStorage.setItem("movComment",JSON.stringify(movieComment))
-  })
+    localStorage.setItem("movComment", JSON.stringify(movieComment))
+  }, [movieComment])
 
-  
-  const getMovies = async () => {
+
+  const getMovies = () => {
     setLoading(true)
-    const getData = await axios.get("http://localhost:3001/movies")
-    setFilm(getData.data)
-    setLoading(false)
+    axios.get("http://localhost:3001/movies")
+      .then((response) => setFilm(response.data))
+      .catch(() => { })
+      .finally(() => setLoading(false))
 
   }
 
   const detailMovie = async (id) => {
     await axios.put(`http://localhost:3001/movies/${id}`)
   }
- 
-  const movieItem = ['Hepsi', ...new Set(film.map(mov =>  mov.kind))]
+
+  const movieItem = ['Hepsi', ...new Set(film.map(mov => mov.kind))]
 
   function getFilteredList() {
     if (selectedCategory === "Hepsi") {
@@ -72,7 +71,7 @@ export default function App() {
     }
 
     return film.filter((item) => item.kind === selectedCategory)
-    
+
   }
 
 
@@ -87,9 +86,9 @@ export default function App() {
 
   const addToFavorite = (id) => {
     const newFavorite = film.find(item => item.id === id)
-  
+
     if (newFavorite) {
-      setFavorite([...favorite, newFavorite ])
+      setFavorite([...favorite, newFavorite])
       toast.success("Film Eklendi")
     }
 
@@ -108,7 +107,7 @@ export default function App() {
 
 
   return (
-    <div >
+    <div>
       <Header
         searchMovie={searchMovie}
         favorite={favorite}
@@ -118,18 +117,18 @@ export default function App() {
         <Routes>
           <Route path='/'
             element={
-            <MovieList
-              search={search}
-              movieItem={movieItem}
-              handleCategoryChange={handleCategoryChange}
-              favorite={favorite}
-              film={film}
-              deleteToFavorite={deleteToFavorite}
-              setFilm={setFilm}
-              filteredList={filteredList}
-              addToFavorite={addToFavorite}
-              selectedCategory={selectedCategory}
-            />} >
+              <MovieList
+                search={search}
+                movieItem={movieItem}
+                handleCategoryChange={handleCategoryChange}
+                favorite={favorite}
+                film={film}
+                deleteToFavorite={deleteToFavorite}
+                setFilm={setFilm}
+                filteredList={filteredList}
+                addToFavorite={addToFavorite}
+                selectedCategory={selectedCategory}
+              />} >
 
           </Route>
 
@@ -142,22 +141,22 @@ export default function App() {
           <Route path='/detail/:id'
             element={
               <Detail
-              comments={comments}
-              setComments={setComments}
-              movieComment={movieComment}
-              setMovieComment={setMovieComment}
-              setFilm={setFilm}
-               film={film}
-              favorite={favorite}
-                addToFavorite={addToFavorite} 
-                  deleteToFavorite={deleteToFavorite}
+                comments={comments}
+                setComments={setComments}
+                movieComment={movieComment}
+                setMovieComment={setMovieComment}
+                setFilm={setFilm}
+                film={film}
+                favorite={favorite}
+                addToFavorite={addToFavorite}
+                deleteToFavorite={deleteToFavorite}
                 detailMovie={(id, movie) => {
                   detailMovie(id, movie)
                 }} />} />
 
           <Route path='/favorite'
             element={<LikeMovie
-            search={search}
+              search={search}
               deleteAllFavorite={deleteAllFavorite}
               favorite={favorite}
               deleteToFavorite={deleteToFavorite}
@@ -167,5 +166,5 @@ export default function App() {
         </Routes>
       }
     </div>
-    )
+  )
 }
